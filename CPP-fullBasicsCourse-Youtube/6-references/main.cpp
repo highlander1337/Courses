cb3cd99de9cc2b85846436adfc35c0d7f10d1d9b
highlander1337 <tassioleno@gmail.com>
2023-10-05 01:34:13 -0300

    Updating CPP-fullBasicsCourse-Youtube files

--- CPP-fullBasicsCourse-Youtube/6-references/main.cpp
+++ CPP-fullBasicsCourse-Youtube/6-references/main.cpp
@@ -10,6 +10,7 @@ int main() {
   std::cout << "myIntVar : " << &myIntVar << std::endl;
   std::cout << "myIntVarReference : " << &myIntVarReference << std::endl;
 
+  std::cout << "<=========================================>" << std::endl;
   std::cout << "Changing the value of myIntVar by reference" << std::endl;
 
   myIntVarReference = 10;
@@ -17,5 +18,38 @@ int main() {
   std::cout << "myIntVar : " << myIntVar << std::endl;
   std::cout << "myIntVarReference : " << myIntVarReference << std::endl;
 
+  std::cout << "<======================================>" << std::endl;
+  std::cout << "Difference between pointers vs reference" << std::endl;
+  // Pointers (useful to not modify the original value)
+  // vs Reference (much more readable)
+  double myDouble{12.32};
+  double &myRefDouble{myDouble};
+  double *myPointerDouble{&myDouble};
+
+  std::cout << "myDouble : " << myDouble << std::endl;
+  std::cout << "myRefDouble : " << myRefDouble << std::endl;
+  std::cout << "myPointerDouble : " << myPointerDouble << std::endl;
+  std::cout << "*myPointerDouble : " << *myPointerDouble << std::endl;
+
+  std::cout << "<======================================>" << std::endl;
+  std::cout << "Const reference"
+            << std::endl; // cannot modify myIntVarReference
+  int myIntVar_2{10};
+  const int &myIntVarReference_2{myIntVar_2};
+  std::cout << "myIntVar_2 : " << myIntVar_2 << std::endl;
+  std::cout << "myIntVarReference_2 : " << myIntVarReference_2
+            << std::endl; // do not need deference
+
+  myIntVar_2 = 15;
+  // myIntVarReference_2 = 10; // compiler error
+
+  std::cout << "<======================================>" << std::endl;
+  std::cout << "Const Pointers" << std::endl;
+  // simulating reference behavior with pointers
+  const int *const myIntVarPointer_2{&myIntVar_2};
+
+  std::cout << "myIntVarPointer_2 : " << *myIntVarPointer_2
+            << std::endl; // must go for deference
+  // *myIntVarPointer_2 = 45; // compile error
   return 0;
 }
--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Knowledge acquired by courses on private and free platforms such as Coursera, Ed
 | Commands                                                  | Situation                                                    |
 | --------------------------------------------------------- | ------------------------------------------------------------ |
 | git pull origin [your_branch] --allow-unrelated-histories | When your commits or branches are different from the remote repository |
+| git remote add origin [your_repository]                   | When you need to connect a local repository to a remote one  |
 
 
 
